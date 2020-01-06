952385ead3da38877e300d47b5f25b471adc4898
Anton Khamets <colorfulfool@gmail.com>
2020-01-06 06:57:05 +0300

    Oops

--- lib/spree_i18n/controller_locale_helper.rb
+++ lib/spree_i18n/controller_locale_helper.rb
@@ -1,18 +1,20 @@
 module SpreeI18n
-  extend ActiveSupport::Concern
-  
-  included do
-    before_action :set_locale    
-  end
-  
-  def set_locale
-    I18n.locale = params[:locale] || implied_locale
-  end
+  module ControllerLocaleHelper
+    extend ActiveSupport::Concern
 
-  private
-  
-    def implied_locale
-      request.env.fetch("HTTP_ACCEPT_LANGUAGE", "")
-        .include?("pt") ? "pt" : "en"
-    end    
+    included do
+      before_action :set_locale    
+    end
+
+    def set_locale
+      I18n.locale = params[:locale] || implied_locale
+    end
+
+    private
+
+      def implied_locale
+        request.env.fetch("HTTP_ACCEPT_LANGUAGE", "")
+          .include?("pt") ? "pt" : "en"
+      end    
+  end
 end
